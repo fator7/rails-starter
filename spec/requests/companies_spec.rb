4a659ded2eb0327d689cd8b021d7dc4332c37ab2
Alberto Harres Rocha <albertohrocha@gmail.com>
2016-09-08 13:32:07 -0300

    Fix companies request spec to pass

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Companies", type: :request do
+  before :all do
+    @user = FactoryGirl.create(:user)
+  end
+
+  before :each do
+    @auth_header = @user.create_new_auth_token
+  end
+
   let(:auth_request_header) {
     {
-      'Accept' => 'application/vnd.mycompany+json;version=1'
+      'Accept' => 'application/vnd.mycompany+json;version=1',
+      'access-token' => @auth_header['access-token'],
+      'client' => @auth_header['client'],
+      'uid' => @auth_header['uid']
     }
   }
 
