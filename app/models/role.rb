e6f14de2c9dcac19389c72a0b65ae6de8c3b4a96
Alberto Harres Rocha <albertohrocha@gmail.com>
2016-10-04 18:03:20 -0300

    Add TODO comment

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,5 +4,8 @@ class Role < ApplicationRecord
   has_many :credentials
   has_many :users, through: :credentials
 
+  # TODO: Add something like this to avoid creation of aliases of default roles
+  # validates_format_of :name, with:/\A(?!Admin|Administrator|Guest)[a-z]+\z/
+
   default_scope { where(company_id: Company.current_id) }
 end
